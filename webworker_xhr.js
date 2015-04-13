9ec7bb0ac15646597340e76dc555bbfe09625639
Arthur Edelstein <arthuredelstein@gmail.com>
2015-04-13 16:09:28 -0700

    attempt cross origin stuff

--- webworker_xhr.js
+++ webworker_xhr.js
@@ -1,3 +1,3 @@
 var request = new XMLHttpRequest();
-request.open("get", "https://arthuredelstein.github.io/tordemos/dummy-file.txt", true);
+request.open("get", "dummy-file.txt", true);
 request.send();

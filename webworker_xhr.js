3e2150da54186245e777cef02267dcf0056e22b3
Arthur Edelstein <arthuredelstein@gmail.com>
2015-04-21 15:45:22 -0700

    remove unneeded

--- webworker_xhr.js
+++ webworker_xhr.js
@@ -1,3 +1,8 @@
 var request = new XMLHttpRequest();
 request.open("get", "dummy-file.txt", true);
 request.send();
+console.log("xhr request sent.");
+var blob = new Blob(["test"]);
+var blobURL = URL.createObjectURL(blob);
+console.log(blobURL);
+
